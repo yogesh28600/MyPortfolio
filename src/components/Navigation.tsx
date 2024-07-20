cd9edb9e8e3f807aed7464d545018e6b7cd68720
yogesh kumar <yogesh28600@gmail.com>
2024-07-20 10:01:05 +0530

    added nav bar for mobile view

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import { Navigations } from "../constants";
 import { IoMdMenu } from "react-icons/io";
+import { IoCloseSharp } from "react-icons/io5";
 const Navigation = () => {
+  const [Open, setOpen] = useState(false);
   return (
     <div className="flex w-full justify-between bg-bg-1 px-5 py-3 lg:justify-between lg:px-56">
       <div className="font-playwrite flex items-center">Nandi Yogesh Kumar</div>
-      <div className="flex cursor-pointer items-center hover:opacity-80 md:hidden">
-        <IoMdMenu size={30} />
+      <div
+        onClick={() => {
+          setOpen(!Open);
+        }}
+        className="flex cursor-pointer items-center hover:opacity-80 md:hidden"
+      >
+        {!Open ? <IoMdMenu size={30} /> : <IoCloseSharp size={30} />}
       </div>
-      <ul className="hidden gap-20 md:flex">
+      <ul
+        id="navItems"
+        className={`md:py-0" + duration-800 absolute ${!Open ? "top-[-100%]" : "top-[9%]"} left-[0] flex w-full flex-col gap-y-5 bg-bg-1 px-5 py-5 md:static md:w-auto md:flex-row md:gap-20 md:px-0`}
+      >
         {Navigations.map((navItem, index) => (
           <li key={index}>
             <a href={navItem.url} className="cursor-pointer hover:opacity-80">
